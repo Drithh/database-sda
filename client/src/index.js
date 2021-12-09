3afd528671439197f0fb9f4bd5baeff75eb64c21
Drithh <adrielalfeus@gmail.com>
2021-12-09 22:24:44 +0700

    update http

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render(
-  <HttpsRedirect>
-    <Router>
-      <App />
-    </Router>
-  </HttpsRedirect>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
