b31ba90cddfbb578f8dcc2be27b9e5c880b92183
Drithh <adrielalfeus@gmail.com>
2021-12-12 13:24:26 +0700

    d3, sql

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,7 +1,53 @@
 import { motion } from 'framer-motion';
 import { pageTransition, variants } from './motion';
+import * as d3 from 'd3';
+import React, { useRef, useEffect, useState } from 'react';
 
 const Home = () => {
+  const [data, setData] = useState([
+    25, 30, 35, 40, 45, 50, 55, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+  ]);
+  const svgRef = useRef();
+  useEffect(() => {
+    const w = 400;
+    const h = 100;
+    const svg = d3.select(svgRef.current).attr('width', w).attr('height', h);
+
+    // setting the scaling
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, w]);
+
+    const yScale = d3.scaleLinear().domain([0, h]).range([h, 0]);
+
+    const generateScleLine = d3
+      .line()
+      .x((d, i) => xScale(i))
+      .y(yScale)
+      .curve(d3.curveCardinal);
+
+    const xAxis = d3
+      .axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat((d) => d + 1);
+
+    const yAxis = d3.axisLeft(yScale).ticks(5);
+
+    svg
+      .append('g')
+      .call(xAxis)
+      .attr('transform', 'translate(' + 0 + ',' + h + ')');
+    svg.append('g').call(yAxis);
+    svg
+      .selectAll('.line')
+      .data([data])
+      .join('path')
+      .attr('d', (d) => generateScleLine(d))
+      .attr('fill', 'none')
+      .attr('stroke', '#000');
+  }, [data]);
+
   return (
     <motion.div
       initial="init"
@@ -10,63 +56,12 @@ const Home = () => {
       variants={variants}
       transition={pageTransition}
     >
-      <h1>Home</h1>
-      <p className="w-[1000px] m-auto">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum officia
-        saepe consequatur! Necessitatibus eligendi magni totam veniam
-        voluptatem, beatae, quaerat est aperiam nihil magnam repudiandae enim
-        aut esse dolorum ut ad. Doloribus natus, consectetur beatae vero dolores
-        error nesciunt repudiandae recusandae quos necessitatibus placeat,
-        aperiam quam sunt! Ducimus autem perferendis beatae dignissimos nostrum
-        modi, distinctio a eveniet quos magni atque aspernatur veniam voluptatum
-        eligendi quis dolore ad officiis similique corporis deleniti. Quos
-        voluptate sapiente vel error, natus laborum rem recusandae architecto in
-        quia quisquam obcaecati corrupti fuga odio repellat at ut ratione eius
-        omnis repellendus ullam minus pariatur maiores quibusdam. Voluptas,
-        similique voluptates? Quasi maiores dolorem laborum libero! Eligendi
-        quia pariatur exercitationem, laudantium sequi numquam, deserunt a optio
-        ipsum hic facere, provident officia eos corporis itaque expedita. Quos
-        dignissimos quod eos autem doloremque commodi animi aspernatur, est
-        architecto blanditiis fuga distinctio suscipit, alias libero placeat
-        incidunt neque soluta, maxime sit! Nostrum voluptates fugiat natus
-        consectetur sint perspiciatis omnis dolor iste, ipsam ad officia,
-        commodi explicabo aliquam velit pariatur officiis doloribus adipisci
-        quas hic eligendi modi atque sapiente alias. Ex deserunt fugit sapiente
-        officiis porro reprehenderit sequi repellendus numquam? Doloribus libero
-        maxime quo. Dolores nihil, culpa et impedit, at dicta doloremque odit
-        itaque, animi labore velit neque totam nostrum quaerat tempore quasi
-        vero inventore laudantium omnis recusandae nisi minus non. Quod
-        temporibus eveniet magnam! Incidunt eveniet a magni quae odio tempora ut
-        rerum tenetur nemo distinctio nihil, ullam doloremque nesciunt cumque
-        facilis esse nostrum fugiat reprehenderit tempore, quia earum ipsam.
-        Reiciendis officia animi, commodi et mollitia ut reprehenderit nostrum
-        maxime molestias culpa nesciunt ducimus, neque quidem ea officiis fuga
-        nemo eos repudiandae cum architecto. Facere, molestias corrupti libero
-        optio magni adipisci? Nihil id consequuntur aperiam itaque earum soluta
-        voluptate, numquam quibusdam eos placeat quisquam libero adipisci
-        eligendi excepturi sint hic inventore minus, quas possimus perferendis
-        repudiandae. Eligendi deserunt praesentium dolores magni earum, ab
-        voluptatum dolore odio dignissimos laboriosam eos quis vel! Commodi
-        vitae cum est quisquam voluptatibus, architecto fuga unde laboriosam
-        adipisci repudiandae. Porro aperiam eveniet maiores perferendis officia
-        illum sunt obcaecati repudiandae ad eos suscipit, vel incidunt dicta
-        vitae corporis. Eius vero recusandae, optio dolore distinctio ducimus
-        alias, tempora quis libero laboriosam itaque, praesentium voluptates
-        adipisci sunt magni necessitatibus earum a sint numquam? Sed animi
-        aperiam labore velit exercitationem veritatis laboriosam cupiditate odio
-        error molestiae dolor, veniam culpa minima ipsum, doloremque qui dolore
-        magnam blanditiis? Eligendi distinctio minus porro, ex voluptate
-        incidunt quibusdam quae suscipit provident nihil, dolorum eos dolorem
-        possimus accusamus. Impedit alias similique accusamus? Libero quisquam
-        laborum qui modi necessitatibus dolor quia quo aut iure dicta corrupti
-        possimus facere sed quasi, error repellat aperiam eum neque incidunt at
-        fugiat autem animi ipsa! Ea, doloribus eius hic nam, quod placeat amet,
-        voluptatibus illum temporibus dicta consequatur praesentium similique
-        atque eaque! Hic perspiciatis alias necessitatibus in sint dolores sunt
-        totam ipsa laudantium non! Quas libero voluptatem, alias unde at est
-        commodi sunt reiciendis consequuntur aperiam molestiae tempora illum!
-        Fuga, cum odit odio iste dicta saepe ad enim dolore eveniet minus.
-      </p>
+      <div className="w-[1024px] m-auto">
+        <svg
+          className="w-full left-6 relative h-full overflow-visible"
+          ref={svgRef}
+        ></svg>
+      </div>
     </motion.div>
   );
 };
---
+++ server/sql/Schema.sql
@@ -0,0 +1,195 @@
+USE master
+GO
+IF EXISTS (SELECT *
+FROM sys.databases
+WHERE NAME = 'DATAVERSE')
+BEGIN
+    ALTER DATABASE DATAVERSE SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DATAVERSE;
+END
+GO
+CREATE DATABASE DATAVERSE
+GO
+
+IF EXISTS (SELECT *
+FROM sys.server_principals
+WHERE name = 'web')
+DROP LOGIN web
+GO
+CREATE LOGIN web WITH PASSWORD=   N'Kenapa03,'
+GO
+USE DATAVERSE;
+GO
+DROP USER web
+CREATE USER web FOR LOGIN web
+GO
+GRANT SELECT, DELETE, INSERT,UPDATE ON DATABASE::DATAVERSE TO web
+GO
+
+CREATE TABLE [dbo].[SUMBER_DAYA_ALAM]
+(
+    Id INT NOT NULL PRIMARY KEY,
+    NamaKomoditi NVARCHAR(20) NOT NULL,
+    NamaEnglish NVARCHAR(20) NOT NULL,
+    Jenis NVARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE [dbo].[KEGUNAAN]
+(
+    Id INT NOT NULL,
+    Kegunaan NVARCHAR(20) NOT NULL,
+    FOREIGN KEY (Id) REFERENCES SUMBER_DAYA_ALAM(Id),
+    PRIMARY KEY (Id, Kegunaan),
+);
+
+
+CREATE TABLE [dbo].[KEHUTANAN]
+(
+    Id INT NOT NULL,
+    FOREIGN KEY (Id) REFERENCES [dbo].[SUMBER_DAYA_ALAM](Id),
+    Famili NVARCHAR(20) NOT NULL,
+    KesesuaianEkologis NVARCHAR(20) NOT NULL,
+    KepadatanKayu SMALLINT NOT NULL,
+    PERTUMBUHAN SMALLINT NOT NULL,
+);
+
+CREATE TABLE [dbo].[PERKEBUNAN]
+(
+    Id INT NOT NULL,
+    FOREIGN KEY (Id) REFERENCES [dbo].[SUMBER_DAYA_ALAM](Id),
+    Famili NVARCHAR(20) NOT NULL,
+    SuhuOptimal FLOAT NOT NULL,
+    WaktuPanen INT NOT NULL,
+    JenisTanaman NVARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE [dbo].[PERTAMBANGAN]
+(
+    Id INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (Id) REFERENCES [dbo].[SUMBER_DAYA_ALAM](Id),
+    IdGolongan NVARCHAR(20) NOT NULL,
+    Asal NVARCHAR(20) NOT NULL,
+    Golongan NVARCHAR(25) NOT NULL,
+);
+
+CREATE TABLE [dbo].[MINERAL]
+(
+    Id INT NOT NULL,
+    FOREIGN KEY (Id) REFERENCES [dbo].[PERTAMBANGAN](Id),
+    Kilau NVARCHAR(20) NOT NULL,
+    KlasifikasiMineral NVARCHAR(20) NOT NULL,
+    KekerasanMohs FLOAT NOT NULL,
+    SistemKristal NVARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE [dbo].[TANAH]
+(
+    Id INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (Id) REFERENCES [dbo].[PERTAMBANGAN](Id),
+    Ukuran FLOAT NOT NULL,
+    JenisTanah INT NOT NULL,
+);
+
+CREATE TABLE [dbo].[KANDUNGAN_TANAH]
+(
+    Id INT NOT NULL,
+    FOREIGN KEY (Id) REFERENCES [dbo].[TANAH](Id),
+    Kandungan NVARCHAR(20) NOT NULL,
+    PRIMARY KEY (Id, Kandungan),
+);
+
+CREATE TABLE [dbo].[MINYAK_BUMI]
+(
+    Id INT NOT NULL,
+    FOREIGN KEY (Id) REFERENCES [dbo].[PERTAMBANGAN](Id),
+    TitikDidih SMALLINT NOT NULL,
+    JumlahAtom SMALLINT NOT NULL,
+);
+
+CREATE TABLE [dbo].[WILAYAH]
+(
+    Id INT NOT NULL PRIMARY KEY,
+    Kota NVARCHAR(30) NOT NULL,
+    Provinsi NVARCHAR(30) NOT NULL,
+    UnitGeografis NVARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE [dbo].[PERUSAHAAN]
+(
+    Id INT NOT NULL PRIMARY KEY,
+    Nama NVARCHAR(20) NOT NULL,
+    Milik NVARCHAR(10) NOT NULL,
+    Sektor NVARCHAR(10) NOT NULL,
+    TahunBerdiri SMALLINT NOT NULL,
+    Alamat NVARCHAR(40) NOT NULL,
+    Jenis NVARCHAR(20) NOT NULL,
+    IdInduk INT,
+    FOREIGN KEY (IdInduk) REFERENCES [dbo].[PERUSAHAAN](Id),
+    IdWilayah INT NOT NULL,
+    FOREIGN KEY (IdWilayah) REFERENCES [dbo].[WILAYAH](Id),
+);
+
+CREATE TABLE [dbo].[TELEPON]
+(
+    Id INT NOT NULL,
+    FOREIGN KEY (Id) REFERENCES [dbo].[PERUSAHAAN](Id),
+    Telepon NVARCHAR(15) NOT NULL,
+    PRIMARY KEY (Id, Telepon),
+);
+
+CREATE TABLE [dbo].[PERUSAHAN_SWASTA]
+(
+    Id INT NOT NULL,
+    FOREIGN KEY (Id) REFERENCES [dbo].[PERUSAHAAN](Id),
+    IzinBerlaku DATE NOT NULL,
+    IzinBerakhir DATE NOT NULl,
+    idPerusahaanBeli INT,
+    FOREIGN KEY (idPerusahaanBeli) REFERENCES [dbo].[PERUSAHAAN](Id),
+    NamaOlahan NVARCHAR(20),
+);
+
+CREATE TABLE [dbo].[BERADA_DI]
+(
+    IdKota INT NOT NULL,
+    FOREIGN KEY (IdKota) REFERENCES [dbo].[WILAYAH](Id),
+    IdKomoditi INT NOT NULL,
+    FOREIGN KEY (IdKomoditi) REFERENCES [dbo].[PERTAMBANGAN](Id),
+    PRIMARY KEY (IdKota, IdKomoditi),
+    PejabatBertanggungJawab NVARCHAR(20) NOT NULL,
+    Angka BIGINT NOT NULL,
+    Satuan NVARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE [dbo].[MENGOLAH]
+(
+    IdKota INT NOT NULL,
+    FOREIGN KEY (IdKota) REFERENCES [dbo].[WILAYAH](Id),
+    IdKomoditi INT NOT NULL,
+    FOREIGN KEY (IdKomoditi) REFERENCES [dbo].[SUMBER_DAYA_ALAM](Id),
+    IdPerusahaan INT NOT NULL,
+    FOREIGN KEY (IdPerusahaan) REFERENCES [dbo].[PERUSAHAAN](Id),
+    PRIMARY KEY (IdKota, IdKomoditi, IdPerusahaan),
+    Luas BIGINT NOT NULL,
+    Longitude FLOAT NOT NULL,
+    Altitude FLOAT NOT NULL,
+    Latitude FLOAT NOT NULL,
+    JenisPengolahan NVARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE [dbo].[HASIL]
+(
+    IdKota INT NOT NULL,
+    IdKomoditi INT NOT NULL,
+    IdPerusahaan INT NOT NULL,
+    FOREIGN KEY (IdKota, IdKomoditi, IdPerusahaan) REFERENCES MENGOLAH(IdKota, IdKomoditi, IdPerusahaan),
+    Tahun SMALLINT NOT NULL,
+    Angka BIGINT NOT NULL,
+    Pendapatan BIGINT NOT NULL,
+    Satuan NVARCHAR(20) NOT NULL,
+    PRIMARY KEY (IdKota, IdKomoditi, IdPerusahaan, Tahun, Angka),
+);
+
+
+
+
+ 
